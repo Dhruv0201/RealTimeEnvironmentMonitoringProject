698ab5dba36565a875bc7784926999cd29fb3ad8
Dhruv <dkhoradiya3@gmail.com>
2023-06-25 20:25:53 +0530

    Changed Style

--- script.js
+++ script.js
@@ -20,7 +20,7 @@
             const parentItem = document.createElement('ul');
                 parentItem.textContent = createdAt[i];
               const childItem = document.createElement('li');
-              childItem.textContent = temperatureData[i];
+              childItem.textContent =( temperatureData[i]*1).toFixed(1);
               parentItem.appendChild(childItem);
             
           
@@ -31,6 +31,7 @@
           // Handle any errors that occurred during the fetch
           console.error("Error:", error);
         });
+        const options = { weekday: "long" };
 
         fetch("https://thingspeak.com/channels/2155544/field/3.json?results=10")
         .then((response) => response.json())
@@ -40,17 +41,17 @@
           for (var i = 0; i < data.feeds.length; i++) {
             humidity.push(data.feeds[i].field3);
             const date = new Date(data.feeds[i].created_at);
-            const options = { weekday: "long" };
             const dayOfWeek = date.toLocaleDateString("en-US", options);
             humiditycreatedat.push(dayOfWeek);
            
           }
+
           const humiditycontainer = document.getElementById("humidity")
           for (let i = 0; i < humiditycreatedat.length; i++) {
             const parentItem = document.createElement('ul');
                 parentItem.textContent = humiditycreatedat[i];
               const childItem = document.createElement('li');
-              childItem.textContent = humidity[i];
+              childItem.textContent = (humidity[i]*1).toFixed(1);
               parentItem.appendChild(childItem);
             
           
