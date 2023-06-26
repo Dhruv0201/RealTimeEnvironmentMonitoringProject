ffa1f7357d9664ad52947d17c58f61dd741c423e
Dhruv <dkhoradiya3@gmail.com>
2023-06-26 18:58:26 +0530

    Added Air Quality

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const options = { weekday: "long" };
  fetch("https://thingspeak.com/channels/2155544/field/2.json?results=10")
         .then((response) => response.json())
         .then((data) => {
           var temperatureData = [];
           var createdAt = [];
+          // const options = { weekday: "long" };
           
 
           for (var i = 0; i < data.feeds.length; i++) {
             temperatureData.push(data.feeds[i].field2);
             const date = new Date(data.feeds[i].created_at);
-            const options = { weekday: "long" };
             const dayOfWeek = date.toLocaleDateString("en-US", options);
             createdAt.push(dayOfWeek);
           }
@@ -31,7 +32,6 @@
           // Handle any errors that occurred during the fetch
           console.error("Error:", error);
         });
-        const options = { weekday: "long" };
 
         fetch("https://thingspeak.com/channels/2155544/field/3.json?results=10")
         .then((response) => response.json())
@@ -58,4 +58,28 @@
             humiditycontainer.appendChild(parentItem);
           }
         })
+        fetch("https://thingspeak.com/channels/2155544/field/3.json?results=10").then((response) => response.json()).then((data)=>{
+          var airquality = [];
+          var created_at = [];
+          for(var i = 0;i<data.feeds.length;i++){
+            airquality.push(data.feeds[i].field3);
+            const date = new Date(data.feeds[i].created_at);
+            const day = date.toLocaleDateString("en-Us",options);
+            created_at.push(day);
+            
+          }
+       
+          const container = document.getElementById("airquality");
+          for(let j = 0 ;j<airquality.length;j++){
+            const ul = document.createElement('ul');
+            ul.textContent = created_at[j];
+            const li = document.createElement('li');
+            li.textContent =( airquality[j]*1).toFixed(1);
+            ul.appendChild(li);
 
+            container.appendChild(ul)
+          }
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
